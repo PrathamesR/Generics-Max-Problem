42b6e5c3ba661c32cb449ebaeb7a0fb8dfb5b0eb
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-21 02:37:04 +0530

    [Prathamesh Rajput] Add. Completed use Case 4

--- GenericsMaxProblem/GetMaxClass.cs
+++ GenericsMaxProblem/GetMaxClass.cs
@@ -8,21 +8,24 @@ namespace GenericsMaxProblem
 {
     public class GetMaxClass<T> where T : IComparable
     {
-        public T no1;
-        public T no2;
-        public T no3;
+        public T[] nos;
 
-        public GetMaxClass(T no1, T no2, T no3)
+        public GetMaxClass(T[] nos)
         {
-            this.no1 = no1;
-            this.no2 = no2;
-            this.no3 = no3;
+            this.nos = nos;
         }
 
         public T GetMax()
         {
-            T temp = no1.CompareTo(no2) > 0 ? no1 : no2;
-            return temp.CompareTo(no3) > 0 ? temp : no3;
+            return nos.Max();
+        }
+    }
+
+    public class MainClass
+    { 
+        static void Main()
+        {
+
         }
     }
 }
--- TestCases/UnitTest1.cs
+++ TestCases/UnitTest1.cs
@@ -7,30 +7,30 @@ namespace TestCases
     public class UnitTest1
     {
         [TestMethod]
-        [DataRow(100, 10, 20, 100)]    //TC 1.1
-        [DataRow(10, 100, 20, 100)]    //TC 1.2
-        [DataRow(10, 10, 200, 200)]    //TC 1.3
-        public void TestCase1(int x, int y, int z, int expected)
+        [DataRow(new int[] {100, 10, 20}, 100)]    //TC 1.1
+        [DataRow(new int[] { 10, 100, 20}, 100)]    //TC 1.2
+        [DataRow(new int[] { 10, 10, 200 }, 200)]    //TC 1.3
+        public void TestCase1(int[] arr, int expected)
         {
-            int actual = new GetMaxClass<int>(x,y,z).GetMax();
+            int actual = new GetMaxClass<int>(arr).GetMax();
             Assert.AreEqual(actual, expected);
         }
 
         [TestMethod]
-        [DataRow(10.0f, 1.1f, 2.3f, 10.0f)]    //TC 2.1
-        [DataRow(1.6f, 10.1f, 2.03f, 10.1f)]    //TC 2.2
-        [DataRow(1.4f, 10.3f, 20.8f, 20.8f)]    //TC 2.3
-        public void TestCase2(float x, float y, float z, float expected)
+        [DataRow(new float[] { 10.0f, 1.1f, 2.3f }, 10.0f)]    //TC 2.1
+        [DataRow(new float[] { 1.6f, 10.1f, 2.03f }, 10.1f)]    //TC 2.2
+        [DataRow(new float[] { 1.4f, 10.3f, 20.8f }, 20.8f)]    //TC 2.3
+        public void TestCase2(float[] arr, float expected)
         {
-            float actual = new GetMaxClass<float>(x, y, z).GetMax();
+            float actual = new GetMaxClass<float>(arr).GetMax();
             Assert.AreEqual(actual, expected);
         }
         
         [TestMethod]
-        [DataRow("1", "2", "3", "3")]    //TC 3
-        public void TestCase3(string x, string y, string z, string expected)
+        [DataRow(new string[] { "1", "2", "3" }, "3")]    //TC 3
+        public void TestCase3(string[] arr, string expected)
         {
-            string actual = new GetMaxClass<string>(x, y, z).GetMax();
+            string actual = new GetMaxClass<string>(arr).GetMax();
             Assert.AreEqual(actual, expected);
         }
     }
